dc33f21047cb9e242e018384b6159e3f57374c4e
malik672 <aremumalik01@gmail.com>
2023-07-04 23:44:42 +0100

    initialize

---
+++ src/XYZ.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+
+
+contract XYZContract is ERC20, AccessControl, ReentrancyGuard, Pausable{
+    
+}
